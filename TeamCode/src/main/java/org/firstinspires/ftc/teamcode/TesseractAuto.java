63711a3e12fc0ea53b7902ed86c4d5a4b2c79163
Matt Kelsey <mattkelseymk@gmail.com>
2017-11-07 17:57:24 -0800

    Refactor autonomous (physically the same, just rewritten and optimized)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TesseractAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TesseractAuto.java
@@ -5,8 +5,6 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
@@ -16,85 +14,210 @@ import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
  */
 @Autonomous(name = "LegitSauceAutoSauce")
 public class TesseractAuto extends LinearOpMode {
-    DcMotor lift;
+    private DcMotor lift;
+    private Servo rTentacle;
+    private Servo lTentacle;
+    private MechanumChassis m;
+    private Vector2D moveVec;
+
+    private ColorSensor jsL;
+    private ColorSensor jsR;
+    private ColorSensor js;
+    private boolean jsConnected = false;
+
+    private static final int TWEEN_TIME = 700;
+
+    private VuforiaHelper vHelper;
+
+    private StartPosition startPos = StartPosition.RED_B;
+    private int sideModifier = 1;
+
     @Override
     public void runOpMode() throws InterruptedException {
-        Servo rTentacle = hardwareMap.servo.get("rTentacle");
-        Servo lTentacle = hardwareMap.servo.get("lTentacle");
-
-        rTentacle.setPosition(ServoValue.RIGHT_TENTACLE_UP);
-        lTentacle.setPosition(ServoValue.LEFT_TENTACLE_UP);
-
-        lift = hardwareMap.dcMotor.get("lift");
-        lift.setDirection(DcMotor.Direction.REVERSE);
-
-        MechanumChassis m = new MechanumChassis(
-            hardwareMap.dcMotor.get("m0"),
-            hardwareMap.dcMotor.get("m1"),
-            hardwareMap.dcMotor.get("m2"),
-            hardwareMap.dcMotor.get("m3"),
-            hardwareMap.get(BNO055IMU.class, "imu"),
-            this
-        );
-
-        m.debugModeEnabled = true;
-
-        // Set global tween time.
-        m.setTweenTime(700);
-
-        telemetry.log().add("Initializing Vuforia");
-        VuforiaHelper vHelper = new VuforiaHelper(this);
-        telemetry.log().add("Done Initializing Vuforia");
-
-
-        char pos = 'A';
-        String side = "RED";
-        int sideModifier = 1;
-        ColorSensor js = hardwareMap.colorSensor.get("jsRight");
-        boolean jsConnected = false;
-        while(!isStarted()) {
-
-            // Side and position configuration.
-            if(gamepad1.a) {
-                pos = 'A';
-                sleep(300);
-            } else if (gamepad1.b) {
-                pos = 'B';
-                sleep(300);
-            } else if (gamepad1.x) {
-                side = "RED";
-                js = hardwareMap.colorSensor.get("jsRight");
-                sideModifier = 1;
-                sleep(300);
-            } else if (gamepad1.y) {
-                side = "BLUE";
-                js = hardwareMap.colorSensor.get("jsLeft");
-                sideModifier = -1;
-                sleep(300);
-            }
-
-            // Check for real values from the color sensor... This will catch and unplugged or misconfigured sensor.
-            if(js.red() != 255 && js.red() != 0 && js.blue() != 0 && js.red() != 255) {
-                jsConnected = true;
-            }
-
-            // Update the drive team.
-            telemetry.addData("Position: ", pos);
-            telemetry.addData("Side: ", side);
-            telemetry.addData("Jewel Color Sensor Connected ", jsConnected);
-            telemetry.update();
-        }
-
+        initialize();
+        configurationLoop();
         waitForStart();
 
         // Process the VuMark
         RelicRecoveryVuMark mark = vHelper.getVuMark();
-
         telemetry.log().add("DETECTED COLUMN: " + mark);
 
+        // Kick the jewel off.
+        doJewel();
+
+        if (startPos == StartPosition.BLUE_A || startPos == StartPosition.RED_A) {
+            switch (mark) {
+                case LEFT:
+                    m.run(1900, 0, 1);
+                    break;
+                case CENTER:
+                    m.run(1500, 0, 1);
+                    break;
+                case RIGHT:
+                    m.setTweenTime(0);
+                    m.run(600, 0, 1);
+                    m.setTweenTime(TWEEN_TIME);
+                    break;
+            }
+        } else {
+            telemetry.log().add("Executing on position B");
+            moveVec.SetComponents(0, 1);
+            m.setDirectionVector(moveVec);
+            switch (mark) {
+                case LEFT:
+                    if (startPos == StartPosition.BLUE_A || startPos == StartPosition.BLUE_B) {
+                        telemetry.log().add("CASE RIGHT");
+                        m.run(1750, 0, 1);
+                        telemetry.log().add("Finished running, starting turn");
+                        m.setRotationTarget(-90*sideModifier);
+                        m.turnToTarget();
+                    } else {
+                        telemetry.log().add("CASE LEFT");
+                        m.run(2400, 0, 1);
+                        m.setRotationTarget(-90 * sideModifier);
+                        m.turnToTarget();
+                        moveVec.SetComponents(-1 * sideModifier, 0);
+                        m.setDirectionVector(moveVec);
+                        m.setTweenTime(0);
+                        m.run(600, 0, 1);
+                        m.setTweenTime(TWEEN_TIME);
+                    }
+                    break;
+                case CENTER:
+                    telemetry.log().add("CASE CENTER");
+                    m.run(2400, 0, 1);
+                    telemetry.log().add("Finished running, starting turn");
+                    m.setRotationTarget(-90*sideModifier);
+                    m.turnToTarget();
+                    break;
+                case RIGHT:
+                    if (startPos == StartPosition.BLUE_A || startPos == StartPosition.BLUE_B) {
+                        telemetry.log().add("CASE LEFT");
+                        m.run(2400, 0, 1);
+                        m.setRotationTarget(-90 * sideModifier);
+                        m.turnToTarget();
+                        moveVec.SetComponents(-1 * sideModifier, 0);
+                        m.setDirectionVector(moveVec);
+                        m.setTweenTime(0);
+                        m.run(600, 0, 1);
+                        m.setTweenTime(TWEEN_TIME);
+                    } else {
+                        telemetry.log().add("CASE RIGHT");
+                        m.run(1750, 0, 1);
+                        telemetry.log().add("Finished running, starting turn");
+                        m.setRotationTarget(-90 * sideModifier);
+                        m.turnToTarget();
+                    }
+                    break;
+            }
+        }
+        if(startPos == StartPosition.BLUE_A || startPos == StartPosition.RED_A) {
+            moveVec.SetComponents(0, 1);
+            m.setDirectionVector(moveVec);
+            m.run(2200, 0, 1);
+        } else {
+            m.setTweenTime(0);
+            moveVec.SetComponents(0, 1);
+            m.setDirectionVector(moveVec);
+            m.run(300, 0, 1);
+            m.setTweenTime(TWEEN_TIME);
+        }
+
+        depositBlock();
+        m.run(1000, 0, 1);
+        moveVec.SetComponents(0, -1);
+        m.setDirectionVector(moveVec);
+        m.run(1000, 0, 1);
+        depositBlock();
+        moveVec.SetComponents(0, 1);
+        m.setDirectionVector(moveVec);
+        m.run(TWEEN_TIME, 0, 1);
+    }
+
+    void initialize() {
+        // Initialize Vuforia
+        telemetry.log().add("Initializing Vuforia...");
+        vHelper = new VuforiaHelper(this);
+        telemetry.log().add("Done Initializing Vuforia");
+
+        // Get hardware devices
+        rTentacle = hardwareMap.servo.get("rTentacle");
+        lTentacle = hardwareMap.servo.get("lTentacle");
+        lift = hardwareMap.dcMotor.get("lift");
+        lift.setDirection(DcMotor.Direction.REVERSE);
+
+        // Initialize mechanum chassis.
+        m = new MechanumChassis(
+                hardwareMap.dcMotor.get("m0"),
+                hardwareMap.dcMotor.get("m1"),
+                hardwareMap.dcMotor.get("m2"),
+                hardwareMap.dcMotor.get("m3"),
+                hardwareMap.get(BNO055IMU.class, "imu"),
+                this
+        );
+        m.debugModeEnabled = true;
+        // Set global tween time.
+        m.setTweenTime(TWEEN_TIME);
+
         // Strafe off motion setup.
-        Vector2D testVec = new Vector2D(-1*sideModifier, 0);
-        m.setDirectionVector(testVec);
+        moveVec = new Vector2D(-1*sideModifier, 0);
+        m.setDirectionVector(moveVec);
+
+        // Initialize servos up.
+        rTentacle.setPosition(ServoValue.RIGHT_TENTACLE_UP);
+        lTentacle.setPosition(ServoValue.LEFT_TENTACLE_UP);
+
+        jsL = hardwareMap.colorSensor.get("jsLeft");
+        jsR = hardwareMap.colorSensor.get("jsRight");
+    }
+
+    void depositBlock() {
+        lift.setPower(1);
+        sleep(1000);
+        lift.setPower(0);
+    }
+
+    private int getSideCoefficient(StartPosition pos) {
+        return (pos == StartPosition.BLUE_A || pos == StartPosition.BLUE_B) ? -1 : 1;
+    }
+
+    private void hardwareValidation() {
+        // Check for real values from the color sensor... This will catch and unplugged or misconfigured sensor.
+        if(js.red() != 255 && js.red() != 0 && js.blue() != 0 && js.red() != 255) {
+            jsConnected = true;
+        }
+    }
+
+    private void configurationLoop() {
+        while(!isStarted()) {
+            // Side and position configuration.
+            if(gamepad1.a) {
+                startPos = StartPosition.BLUE_A;
+                js = jsL;
+            } else if (gamepad1.b) {
+                startPos = StartPosition.BLUE_B;
+                js = jsL;
+                sideModifier = -1;
+            } else if (gamepad1.x) {
+                startPos = StartPosition.RED_A;
+                js = jsR;
+            } else if (gamepad1.y) {
+                startPos = StartPosition.RED_B;
+                js = jsR;
+            }
+
+            sideModifier = getSideCoefficient(startPos);
+
+            hardwareValidation();
+
+            // Update the drive team.
+            telemetry.addData("Starting Position: ", startPos);
+            telemetry.addData("Jewel Color Sensor Connected ", jsConnected);
+            telemetry.update();
+        }
+    }
+
+    private void doJewel() {
 
         // Lower the tentacles.
         lTentacle.setPosition(ServoValue.LEFT_TENTACLE_DOWN);
@@ -119,99 +242,10 @@ public class TesseractAuto extends LinearOpMode {
         m.turnToTarget();
 
         telemetry.log().add("FINISHED RESETTING TO HOME ROTATION");
-
-        if (pos == 'A') {
-            switch (mark) {
-                case LEFT:
-                    m.run(1900, 0, 1);
-                    break;
-                case CENTER:
-                    m.run(1500, 0, 1);
-                    break;
-                case RIGHT:
-                    m.setTweenTime(0);
-                    m.run(600, 0, 1);
-                    m.setTweenTime(700);
-                    break;
-            }
-        } else {
-            telemetry.log().add("Executing on position B");
-            testVec.SetComponents(0, 1);
-            m.setDirectionVector(testVec);
-            switch (mark) {
-                case LEFT:
-                    if (side.equals("BLUE")) {
-                        telemetry.log().add("CASE RIGHT");
-                        m.run(1750, 0, 1);
-                        telemetry.log().add("Finished running, starting turn");
-                        m.setRotationTarget(-90*sideModifier);
-                        m.turnToTarget();
-                    } else {
-                        telemetry.log().add("CASE LEFT");
-                        m.run(2400, 0, 1);
-                        m.setRotationTarget(-90 * sideModifier);
-                        m.turnToTarget();
-                        testVec.SetComponents(-1 * sideModifier, 0);
-                        m.setDirectionVector(testVec);
-                        m.setTweenTime(0);
-                        m.run(600, 0, 1);
-                        m.setTweenTime(700);
-                    }
-                    break;
-                case CENTER:
-                    telemetry.log().add("CASE CENTER");
-                    m.run(2400, 0, 1);
-                    telemetry.log().add("Finished running, starting turn");
-                    m.setRotationTarget(-90*sideModifier);
-                    m.turnToTarget();
-                    break;
-                case RIGHT:
-                    if (side.equals("BLUE")) {
-                        telemetry.log().add("CASE LEFT");
-                        m.run(2400, 0, 1);
-                        m.setRotationTarget(-90 * sideModifier);
-                        m.turnToTarget();
-                        testVec.SetComponents(-1 * sideModifier, 0);
-                        m.setDirectionVector(testVec);
-                        m.setTweenTime(0);
-                        m.run(600, 0, 1);
-                        m.setTweenTime(700);
-                    } else {
-                        telemetry.log().add("CASE RIGHT");
-                        m.run(1750, 0, 1);
-                        telemetry.log().add("Finished running, starting turn");
-                        m.setRotationTarget(-90 * sideModifier);
-                        m.turnToTarget();
-                    }
-                    break;
-            }
-        }
-        if(pos == 'A') {
-            testVec.SetComponents(0, 1);
-            m.setDirectionVector(testVec);
-            m.run(2200, 0, 1);
-        } else {
-            m.setTweenTime(0);
-            testVec.SetComponents(0, 1);
-            m.setDirectionVector(testVec);
-            m.run(300, 0, 1);
-            m.setTweenTime(700);
-        }
-
-        depositBlock();
-        m.run(1000, 0, 1);
-        testVec.SetComponents(0, -1);
-        m.setDirectionVector(testVec);
-        m.run(1000, 0, 1);
-        depositBlock();
-        testVec.SetComponents(0, 1);
-        m.setDirectionVector(testVec);
-        m.run(700, 0, 1);
     }
 
-    void depositBlock() {
-        lift.setPower(1);
-        sleep(1000);
-        lift.setPower(0);
+    private enum StartPosition {
+        RED_A, RED_B, BLUE_A, BLUE_B
     }
+
 }
